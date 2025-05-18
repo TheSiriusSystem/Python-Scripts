bbd957b48ac7ebc0a3737c40152e83c59c62ac85
TheSiriusSystem <91137432+TheSiriusSystem@users.noreply.github.com>
2025-05-18 20:05:59 +0600

    Create car.py

---
+++ car.py
@@ -0,0 +1,28 @@
+is_car_on: bool = False
+
+if __name__ == "__main__":
+    while True:
+        command: str = input("> ").strip().lower()
+        match command:
+            case "start":
+                if not is_car_on:
+                    is_car_on = True
+                    print("Car started... ready to go!")
+                else:
+                    print("Car is already started!")
+            case "stop":
+                if is_car_on:
+                    is_car_on = False
+                    print("Car stopped.")
+                else:
+                    print("Car is already stopped!")
+            case "exit":
+                print("Goodbye, User!")
+                break
+            case "help":
+                print("start - Start the car.")
+                print("stop - Stop the car.")
+                print("exit - Exit the application.")
+                print("help - See all commands.")
+            case _:
+                print("Sorry, I don't understand that...")
