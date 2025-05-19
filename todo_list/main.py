8dfcbf3033dd48ee28ca3684a35740c3e03e8150
TheSiriusSystem <91137432+TheSiriusSystem@users.noreply.github.com>
2025-05-19 16:42:36 +0600

    Update main.py

--- todo_list/main.py
+++ todo_list/main.py
@@ -2,8 +2,13 @@ import os
 import json
 
 SAVE_FILE: str = "save.json"
+TODO_LIST_LIMIT: int = 25
 
 todo_list: list[str] = []
+
+def save() -> None:
+    with open(SAVE_FILE, "w", encoding="utf-8") as f:
+        json.dump(todo_list, f, indent=4)
 
 def is_save_valid(data) -> bool:
     if type(data) != list:
@@ -18,35 +23,56 @@ if __name__ == "__main__":
         with open(SAVE_FILE, "r", encoding="utf-8") as f:
             data = json.load(f)
             if is_save_valid(data):
-                for element in data:
-                    element = element.lower()
                 todo_list = data
     print("Welcome to the To-Do List Manager! Type 'help' to see available commands.")
     while True:
-        command: str = input("> ").strip().lower()
-        if command.startswith("add "):
-            item: str = command[4:]
-            todo_list.append(item)
-            print(f"Added item '{item}'.")
-        elif command.startswith("remove "):
-            item: str = command[7:]
-            if item == "all":
+        save()
+        command: str = input("> ").strip()
+        command_lowercase: str = command.lower()
+        if command_lowercase.startswith("add "):
+            if len(todo_list) < TODO_LIST_LIMIT:
+                name: str = command[4:]
+                if name not in todo_list:
+                    todo_list.append(name)
+                    print(f"Added item '{name}'.")
+                else:
+                    print(f"'{name}' is already in your to-do list.")
+            else:
+                print(f"You cannot add more than {TODO_LIST_LIMIT} names to the to-do list!")
+        elif command_lowercase.startswith("remove "):
+            name: str = command[7:]
+            if name == "all":
+                count: int = len(todo_list)
                 todo_list.clear()
-            elif item in todo_list:
-                todo_list.pop(todo_list.index(item))
-                print(f"Removed item '{item}'.")
-        elif command == "list":
-            print(todo_list)
-        elif command == "help":
-            print("add <item> - Add a new task.")
-            print("remove <item> | all - Remove a task..")
+                print(f"Removed {count} item(s).")
+            elif name in todo_list:
+                todo_list.pop(todo_list.index(name))
+                print(f"Removed item '{name}'.")
+            elif name.isdigit():
+                index: int = int(name)
+                try:
+                    name = todo_list[index]
+                    todo_list.pop(index)
+                    print(f"Removed item '{name}'.")
+                except (IndexError, ValueError):
+                    print(f"Index {index} is not in your to-do list.")
+            else:
+                print(f"'{name}' is not in your to-do list.")
+        elif command_lowercase == "list":
+            if len(todo_list) > 0:
+                for index, name in enumerate(todo_list):
+                    print(f"Item: (index: {index}, name: {name})")
+            else:
+                print("Your to-do list is empty.")
+        elif command_lowercase == "help":
+            print("add <name> - Add a new task.")
+            print("remove <name> | <index> | all - Remove a task..")
             print("list - Show all tasks.")
             print("help - Show all commands.")
             print("exit - Exit the application.")
-        elif command == "exit":
+        elif command_lowercase == "exit":
             print("Goodbye, User!")
             break
         else:
             print("Invalid command.")
-    with open(SAVE_FILE, "w", encoding="utf-8") as f:
-        json.dump(todo_list, f, indent=4)
+    save()
