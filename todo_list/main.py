9ba7ffda93acf39e431cdbb88531472d797c678e
TheSiriusSystem <91137432+TheSiriusSystem@users.noreply.github.com>
2025-05-20 17:36:58 +0600

    Update main.py

--- todo_list/main.py
+++ todo_list/main.py
@@ -11,8 +11,6 @@ VALID_TODO_LIST_ITEM_STATUSES: list[str] = [
     "done",
 ]
 
-todo_list: list[ToDoListItem] = []
-
 def save() -> None:
     with open(SAVE_FILE, "w", encoding="utf-8") as f:
         json.dump(todo_list, f, indent=4)
@@ -37,76 +35,119 @@ def get_todo_list_item(name: str) -> ToDoListItem | None:
             return item
     return None
 
+def is_argument_not_specified(argument) -> bool:
+    return not isinstance(argument, str) or argument == ""
+
+def is_argument_specified(argument) -> bool:
+    return isinstance(argument, str) and argument != ""
+
+def print_no_command_arg_warning(command: str) -> None:
+    print(f"Command '{command}' does not take a parameter.")
+
+def command_add(argument) -> None:
+    if is_argument_not_specified(argument):
+        print("Usage: <name>")
+        return
+
+    if len(todo_list) < TODO_LIST_LIMIT:
+        if argument != "all" and not get_todo_list_item(argument):
+            todo_list.append({
+                "name": argument,
+                "status": VALID_TODO_LIST_ITEM_STATUSES[0],
+            })
+            print(f"Added item '{argument}'.")
+        else:
+            if argument == "all":
+                print(f"Cannot add an item with the name '{argument}'.")
+            else:
+                print(f"'{argument}' is already in your to-do list.")
+    else:
+        print(f"You cannot add more than {TODO_LIST_LIMIT} names to the to-do list!")
+
+def command_remove(argument) -> None:
+    if is_argument_not_specified(argument):
+        print("Usage: <name>")
+        return
+
+    item = get_todo_list_item(argument) if argument != "all" else None # type: ignore
+    if argument == "all":
+        count: int = len(todo_list)
+        todo_list.clear()
+        print(f"Removed {count} item(s).")
+    elif item:
+        todo_list.pop(todo_list.index(item))
+        print(f"Removed item '{argument}'.")
+    else:
+        print(f"'{argument}' is not in your to-do list.")
+
+def command_mark(argument) -> None:
+    if is_argument_not_specified(argument):
+        print("Usage: <name>")
+        return
+
+    item: ToDoListItem = get_todo_list_item(argument) # type: ignore
+    if item:
+        new_item_status: str = input(f"Enter a status... ").strip().lower()
+        if new_item_status not in VALID_TODO_LIST_ITEM_STATUSES:
+            print("Invalid option.")
+        else:
+            if item["status"] != new_item_status:
+                item["status"] = new_item_status
+                print(f"Marked task '{item["name"]}' as '{new_item_status}'.")
+            else:
+                print(f"Task '{item["name"]}' is already marked as '{new_item_status}'.")
+    else:
+        print(f"'{argument}' is not in your to-do list.")
+
+def command_list(argument) -> None:
+    if is_argument_specified(argument):
+        print_no_command_arg_warning("list")
+        return
+
+    if len(todo_list) > 0:
+        for index, item in enumerate(todo_list):
+            print(f"[{index + 1}]: {item["name"]} - {item["status"].capitalize()}")
+    else:
+        print("Your to-do list is empty.")
+
+def command_help(argument) -> None:
+    if is_argument_specified(argument):
+        print_no_command_arg_warning("help")
+        return
+
+    print("add <name> - Add a new task.")
+    print("remove <name> | all - Remove a task.")
+    print(f"mark <name> - Mark a task as ({", ".join(VALID_TODO_LIST_ITEM_STATUSES)}). When a task is provided, it will prompt you to set a new status.")
+    print("list - Show all tasks.")
+    print("help - Show all commands.")
+    print("exit - Exit the application.")
+
 if __name__ == "__main__":
+    commands: dict = {
+        "add": command_add,
+        "remove": command_remove,
+        "mark": command_mark,
+        "list": command_list,
+        "help": command_help,
+    }
+    todo_list: list[ToDoListItem] = []
+
     if os.path.exists(SAVE_FILE):
         with open(SAVE_FILE, "r", encoding="utf-8") as f:
             data = json.load(f)
             if is_save_valid(data):
                 todo_list = data
-    print("Welcome to the To-Do List Manager! Type 'help' to see available commands.")
+    print("Welcome to the To-Do List Manager!")
+    print("Type 'help' to see available commands.")
     while True:
         save()
-        command: str = input("> ").strip()
-        command_lowercase: str = command.lower()
-        if command_lowercase.startswith("add "):
-            if len(todo_list) < TODO_LIST_LIMIT:
-                name: str = command[4:]
-                if name != "all" and not get_todo_list_item(name):
-                    todo_list.append({
-                        "name": name,
-                        "status": VALID_TODO_LIST_ITEM_STATUSES[0],
-                    })
-                    print(f"Added item '{name}'.")
-                else:
-                    if name == "all":
-                        print(f"Cannot add an item with the name '{name}'.")
-                    else:
-                        print(f"'{name}' is already in your to-do list.")
-            else:
-                print(f"You cannot add more than {TODO_LIST_LIMIT} names to the to-do list!")
-        elif command_lowercase.startswith("remove "):
-            name: str = command[7:]
-            item = get_todo_list_item(name) if name != "all" else None # type: ignore
-            if name == "all":
-                count: int = len(todo_list)
-                todo_list.clear()
-                print(f"Removed {count} item(s).")
-            elif item:
-                todo_list.pop(todo_list.index(item))
-                print(f"Removed item '{name}'.")
-            else:
-                print(f"'{name}' is not in your to-do list.")
-        elif command_lowercase.startswith("mark "):
-            name: str = command[5:]
-            item: ToDoListItem = get_todo_list_item(name) # type: ignore
-            if item:
-                new_item_status: str = input(f"Enter a status... ").strip().lower()
-                if new_item_status not in VALID_TODO_LIST_ITEM_STATUSES:
-                    print("Invalid option.")
-                else:
-                    if item["status"] != new_item_status:
-                        item["status"] = new_item_status
-                        print(f"Marked task '{item["name"]}' as '{new_item_status}'.")
-                    else:
-                        print(f"Task '{item["name"]}' is already marked as '{new_item_status}'.")
-            else:
-                print(f"'{name}' is not in your to-do list.")
-        elif command_lowercase == "list":
-            if len(todo_list) > 0:
-                for index, item in enumerate(todo_list):
-                    print(f"[{index + 1}]: {item["name"]} - {item["status"].capitalize()}")
-            else:
-                print("Your to-do list is empty.")
-        elif command_lowercase == "help":
-            print("add <name> - Add a new task.")
-            print("remove <name> | all - Remove a task.")
-            print(f"mark <name> - Mark a task as ({", ".join(VALID_TODO_LIST_ITEM_STATUSES)}). When a task is provided, it will prompt you to set a new status.")
-            print("list - Show all tasks.")
-            print("help - Show all commands.")
-            print("exit - Exit the application.")
-        elif command_lowercase == "exit":
+        command_parts: list[str] = input("> ").strip().split(maxsplit=1)
+        command: str = command_parts[0].lower()
+        if command == "exit":
             print("Goodbye, User!")
             break
+        elif command in commands:
+            commands[command](command_parts[1] if len(command_parts) == 2 else None)
         else:
             print("Invalid command.")
     save()
