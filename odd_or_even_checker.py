8185c71be85790b7b4e602ae26e890f1df86912a
TheSiriusSystem <91137432+TheSiriusSystem@users.noreply.github.com>
2025-05-22 17:45:41 +0600

    Update odd_or_even_checker.py

--- odd_or_even_checker.py
+++ odd_or_even_checker.py
@@ -1,14 +1,14 @@
 if __name__ == "__main__":
     print("Welcome to the Odd or Even Checker!")
-    print("Type 'exit' to exit the application.")
     while True:
-        command: str = input("> ").strip().lower()
-        if command.isdigit():
-            number: int = int(command)
-            print(f"{number} is {"odd" if number % 2 != 0 else "even"}.")
-        else:
-            if command == "exit":
-                print("Goodbye, User!")
-                break
+        try:
+            command: str = input("> ").strip().lower()
+            if command.isdigit():
+                number: int = int(command)
+                print(f"{number} is {"odd" if number % 2 != 0 else "even"}.")
             else:
                 print("Invalid input. Please enter a number.")
+        except KeyboardInterrupt:
+            print("") # Creates a newline.
+            print("Goodbye, User!")
+            break
